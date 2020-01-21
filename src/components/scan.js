a3c3f877fdef79683988c8bc9527e523582d58d5
Matt Henry <matt.henry@gsa.gov>
2020-01-21 16:43:07 -0500

    add form to filter results

--- src/components/scan.js
+++ src/components/scan.js
@@ -1,17 +1,41 @@
 import React, { useState, useEffect } from "react"
 import Pagination from "./pagination"
 import ScanTable from "./scan-table"
+import QueryForm from "./query-form"
 
 const Scan = ({ scanType }) => {
   const [isLoading, setIsLoading] = useState(true)
   const [scanData, setScanData] = useState({})
   const [currentPage, setCurrentPage] = useState(1)
   const [recordsPerPage, setRecordsPerPage] = useState(50)
+  const [privacyPageQuery, setPrivacyPageQuery] = useState("*")
+
+  const [queryParams, setQueryParams] = useState({
+    page: currentPage,
+    data: {
+      status_code: privacyPageQuery,
+    },
+  })
+
+  const flattenObject = (obj, prefix = "") =>
+    Object.keys(obj).reduce((acc, k) => {
+      const pre = prefix.length ? prefix + "." : ""
+      if (typeof obj[k] === "object")
+        Object.assign(acc, flattenObject(obj[k], pre + k))
+      else acc[pre + k] = obj[k]
+      return acc
+    }, {})
 
   const apiBaseUrl = `https://site-scanning.app.cloud.gov/api/v1/scans/`
 
   const fetchScanData = async () => {
-    const req = new Request(`${apiBaseUrl}${scanType}?page=${currentPage}`, {
+    const flatQueryObj = flattenObject(queryParams)
+    const queryString = Object.entries(flatQueryObj)
+      .map(entry => entry.join("="))
+      .join("&")
+
+    console.log(`${apiBaseUrl}${scanType}/?${queryString}`)
+    const req = new Request(`${apiBaseUrl}${scanType}/?${queryString}`, {
       method: "GET",
     })
     const resp = await fetch(req)
@@ -20,18 +44,30 @@ const Scan = ({ scanType }) => {
     setIsLoading(false)
   }
 
+  const { page } = queryParams
+
   useEffect(() => {
     fetchScanData()
-  }, [recordsPerPage, currentPage])
+    console.log(scanData)
+  }, [currentPage, privacyPageQuery])
 
   const handlePageNav = newPageNumber => {
+    setQueryParams(oldParams =>
+      Object.assign(oldParams, { page: newPageNumber })
+    )
     setCurrentPage(newPageNumber)
+  }
+
+  const handleFilterQuery = newQuery => {
+    setPrivacyPageQuery(newQuery)
+    setQueryParams(Object.assign(queryParams, newQuery))
   }
 
   return isLoading ? (
     <p>Loading…</p>
   ) : (
     <>
+      <QueryForm handleFilterQuery={handleFilterQuery} />
       <ScanTable scanType={scanType} scanData={scanData} />
       <Pagination
         currentPageNumber={currentPage}
