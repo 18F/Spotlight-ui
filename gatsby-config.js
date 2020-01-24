4a348453de7b601e27ab26ff97d239737bfc19f3
Matt Henry <matt.henry@gsa.gov>
2020-01-24 10:01:41 -0500

    Try configuring path for pages dir

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
