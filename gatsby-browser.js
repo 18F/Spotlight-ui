27531f71e27d5ae62485929ac49725ae6d2e6cf0
Matt Henry <matt.henry@gsa.gov>
2020-01-24 11:00:47 -0500

    Move style imports to browser config

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+import "./src/styles/index.scss"
+import "uswds"
+
+import { siteMetadata } from "./gatsby-config"
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,6 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "../styles/index.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
