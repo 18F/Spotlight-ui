6e65c5303715d623ef3d3f16b78cfd0993a17c74
Matt Henry <matt.henry@gsa.gov>
2020-01-28 10:17:49 -0500

    Add `All` option to agency select

--- src/components/query-form.js
+++ src/components/query-form.js
@@ -1,7 +1,19 @@
-import React, { useMemo } from "react"
+import React from "react"
 
 export default ({ agencies, handleFilterQuery }) => {
-  console.log(agencies)
+  const addOptionAll = optionsArr => {
+    const emptyOpt = 'All';
+    return [emptyOpt, ...optionsArr]
+  }
+
+  agencies = addOptionAll(agencies).map(a => {
+    const value = a === `All` ? `*` : a.replace(/ /g, "+");
+    return (
+      <option key={a} value={value}>
+        {a}
+      </option>
+    )
+  })
   return (
     <form>
       <label htmlFor="privacyPagePresent">Privacy Page Present</label>
@@ -17,12 +29,12 @@ export default ({ agencies, handleFilterQuery }) => {
         <option value="*">All</option>
       </select>
       <label htmlFor="agencies">Filter by agency</label>
-      <select id="agencies" name="agencies">
-        {agencies.map(a => (
-          <option key={a} value={a}>
-            {a}
-          </option>
-        ))}
+      <select
+        id="agencies"
+        name="agencies"
+        onChange={e => handleFilterQuery({ agency: e.target.value })}
+      >
+        {agencies}
       </select>
     </form>
   )
