6b1e0c7e2542963dbd611eb91312e3c21501918a
Matt Henry <matt.henry@gsa.gov>
2020-02-28 09:19:17 -0500

    rm unused code

--- src/pages/search200.js
+++ src/pages/search200.js
@@ -1,23 +1,7 @@
 import React from 'react';
 import Scan from '../components/scan';
-import YAMLData from '../data/search200-filters.yml';
-import { customFilterOptions } from '../utils';
-
-const customAgencyFilters = customFilterOptions(YAMLData.filters, 'agency');
-const customBranchFilters = customFilterOptions(YAMLData.filters, 'branch');
-
-// ex: ("Consumer+Financial+Protection+Bureau")OR("Government+Publishing+Office")
-const allAgenciesString = customAgencyFilters
-  ? customAgencyFilters['agency']
-      .map(a => `("${a.replace(/ /g, '+')}")`)
-      .join('OR')
-  : '*';
-
-const allBranchesString = customBranchFilters
-  ? customBranchFilters['branch']
-      .map(b => `("${b.replace(/ /g, '+')}")`)
-      .join('OR')
-  : '*';
+import YAMLData from '../data/config.yml';
+import QueryProvider from '../components/query-provider';
 
 export default () => (
   <div className="grid-container">
@@ -26,23 +10,12 @@ export default () => (
       200 Scans Search Welcome to the Federal website scanner, a project
       developed by 18F! This search will search the 200 scanner results.
     </p>
-    <Scan
-      scanType={'200scanner'}
-      columns={{
-        domain: 'Domain',
-        scantype: 'Scan Type',
-        domaintype: 'Branch',
-        agency: 'Agency',
-        status_code: 'Status Code',
-        scan_data_url: 'Scan Data URL',
-        lastmodified: 'Last Modified',
-      }}
-      defaultQuery={{
-        page: 1,
-        agency: allAgenciesString,
-        domaintype: allBranchesString,
-      }}
-      filters={YAMLData.filters}
-    />
+    <QueryProvider scanType={'200scanner'}>
+      <Scan
+        scanType={'200scanner'}
+        columns={YAMLData.search200.columns}
+        filters={YAMLData.search200.filters}
+      />
+    </QueryProvider>
   </div>
 );
