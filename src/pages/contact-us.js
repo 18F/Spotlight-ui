456da9e9d634387e2617f4cafb21e5cf8c7399ec
Eleni <eleni.gesch-karamanlidis@gsa.gov>
2020-04-13 15:28:29 -0500

    Update contact-us.js

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -6,8 +6,9 @@ import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
+    <SEO title="Contact us" />
+    <h1>Contact us</h1>
+ <h2>Questions on how to use Site Scanner?</h2>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
