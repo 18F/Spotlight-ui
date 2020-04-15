7b96d996fe97a78daa7e71205af9a276105953e5
Matt Henry <matt.henry@gsa.gov>
2020-04-15 09:46:47 -0400

    Implement basic security report

--- src/components/report.js
+++ src/components/report.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+<<<<<<< HEAD
 import React, { useState, useEffect, useContext } from 'react';
 import ReportFilters from './report-filters';
 import { API_BASE_URL } from '../constants';
@@ -44,6 +45,40 @@ const Report = ({ reportType, columns, endpoint }) => {
     setLoading(true);
     fetchReportData();
   }, [queryString, query.scanDate]);
+=======
+import React, { useState, useEffect } from 'react';
+import { API_BASE_URL } from '../constants';
+import axios from 'axios';
+
+const Report = ({ reportType }) => {
+  const [reportData, setReportData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+
+  const columns = [
+    { title: `Domain`, accessor: (obj) => obj.domain },
+    { title: `Agency`, accessor: (obj) => obj.agency },
+    {
+      title: `Domain Supports HTTPS`,
+      accessor: (obj) => obj.data['Domain Supports HTTPS'],
+    },
+    { title: `HSTS`, accessor: (obj) => obj.data.HSTS },
+    {
+      title: `Headers`,
+      accessor: (obj) => obj.data.endpoints.https.headers,
+    },
+  ];
+
+  const fetchReportData = async (page) => {
+    const result = await axios(`${API_BASE_URL}scans/pshtt/?page=${page}`);
+    setReportData(result.data.results);
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    fetchReportData(page);
+  }, [page]);
+>>>>>>> Implement basic security report
 
   useEffect(() => {
     setLoading(false);
@@ -51,12 +86,16 @@ const Report = ({ reportType, columns, endpoint }) => {
 
   return (
     <>
+<<<<<<< HEAD
       <ReportFilters reportType={reportType} />
       <Pagination
         recordCount={recordCount}
         handleFilterQuery={handlePageChange}
       />
       <CsvLink queryUrl={`${queryBaseUrl}${endpoint}/csv/?${queryString}`} />
+=======
+      <h1>{reportType}</h1>
+>>>>>>> Implement basic security report
       <ReportTable>
         <ReportTableHead columns={columns} />
         <ReportTableBody
@@ -70,6 +109,7 @@ const Report = ({ reportType, columns, endpoint }) => {
 };
 
 Report.propTypes = {
+<<<<<<< HEAD
   columns: PropTypes.arrayOf(PropTypes.object),
   endpoint: PropTypes.string,
 };
@@ -92,12 +132,31 @@ ReportTable.propTypes = {
   ]),
 };
 
+=======
+  reportType: PropTypes.string,
+};
+
+Report.defaultProps = {
+  reportType: `Security`,
+};
+
+export default Report;
+
+const ReportTable = ({ children }) => (
+  <table className="usa-table">{children}</table>
+);
+
+>>>>>>> Implement basic security report
 const ReportTableHead = ({ columns }) => {
   return (
     <thead>
       <tr>
         {columns.map((c) => (
+<<<<<<< HEAD
           <th key={uuidv1()} scope="col">
+=======
+          <th key={c.title} scope="col">
+>>>>>>> Implement basic security report
             {c.title}
           </th>
         ))}
@@ -106,22 +165,30 @@ const ReportTableHead = ({ columns }) => {
   );
 };
 
+<<<<<<< HEAD
 ReportTableHead.propTypes = {
   columns: PropTypes.arrayOf(PropTypes.object),
 };
 
+=======
+>>>>>>> Implement basic security report
 const ReportTableBody = ({ columns, records, isLoading }) => {
   return isLoading ? (
     <tbody></tbody>
   ) : (
     <tbody>
       {records.map((r) => (
+<<<<<<< HEAD
         <ReportTableRow key={uuidv1()} columns={columns} record={r} />
+=======
+        <ReportTableRow columns={columns} record={r} />
+>>>>>>> Implement basic security report
       ))}
     </tbody>
   );
 };
 
+<<<<<<< HEAD
 ReportTableBody.propTypes = {
   columns: PropTypes.arrayOf(PropTypes.object),
   records: PropTypes.arrayOf(PropTypes.object),
@@ -137,11 +204,19 @@ const ReportTableRow = ({ columns, record }) => {
           value={c.accessor(record)}
           isFirst={i == 0}
         />
+=======
+const ReportTableRow = ({ columns, record }) => {
+  return (
+    <tr>
+      {columns.map((c) => (
+        <ReportTableCell value={c.accessor(record)} />
+>>>>>>> Implement basic security report
       ))}
     </tr>
   );
 };
 
+<<<<<<< HEAD
 ReportTableRow.propTypes = {
   columns: PropTypes.arrayOf(PropTypes.object),
   record: PropTypes.object,
@@ -180,12 +255,35 @@ const ObjectList = ({ object }) => {
       {Object.keys(object).map((k, i) => (
         <li key={uuidv1()}>
           <strong>{k}:</strong> {object[k]}
+=======
+const ReportTableCell = ({ value }) => {
+  const parseValue = (value) => {
+    if (typeof value == 'boolean') return String(value);
+    if (typeof value == 'object') return <HeadersList headers={value} />;
+    return value;
+  };
+  console.log(parseValue(value));
+
+  return <td>{parseValue(value)}</td>;
+};
+
+const HeadersList = ({ headers }) => {
+  console.log(headers);
+  return (
+    <ul>
+      {Object.keys(headers).map((k) => (
+        <li>
+          <strong>{k}:</strong> {headers[k]}
+>>>>>>> Implement basic security report
         </li>
       ))}
     </ul>
   );
 };
+<<<<<<< HEAD
 
 ObjectList.propTypes = {
   record: PropTypes.object,
 };
+=======
+>>>>>>> Implement basic security report
