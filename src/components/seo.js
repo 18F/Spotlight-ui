1050c0aec685e15ee565e0820acade523756d167
Gray Brooks <gbinal@users.noreply.github.com>
2020-04-20 14:30:13 -0400

    adding DAP

--- src/components/seo.js
+++ src/components/seo.js
@@ -68,7 +68,9 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+      >
+      <script async type="text/javascript" id="_fed_an_ua_tag" src="https://dap.digitalgov.gov/Universal-Federated-Analytics-Min.js?agency=GSA"></script>
+    </Helmet>
   )
 }
 
