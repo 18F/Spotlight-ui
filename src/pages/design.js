9a0d8026b384cf81a6160b2fe1f2e027ea7532ff
Matt Henry <matt.henry@gsa.gov>
2020-04-20 16:13:10 -0400

    Wire up reducer & context

---
+++ src/pages/design.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Report from '../components/report';
+import Layout from '../components/layout';
+import ReportQueryProvider from '../components/report-query-provider';
+
+const columns = [
+  { title: `Domain`, accessor: (obj) => obj.domain },
+  { title: `Agency`, accessor: (obj) => obj.agency },
+  { title: `USWDS Indicators`, accessor: (obj) => obj.data.total_score },
+  {
+    title: `USWDS Version`,
+    accessor: (obj) => obj.data.uswdsversion,
+  },
+];
+
+export default () => (
+  <Layout>
+    <div className="grid-container">
+      <h1>Design</h1>
+      <p>
+        This report contains scan results pertaining to CISA requirements and
+        21st Century IDEA act security requirements
+      </p>
+    </div>
+
+    <ReportQueryProvider>
+      <Report columns={columns} endpoint={'scans/uswds2'} />
+    </ReportQueryProvider>
+  </Layout>
+);
