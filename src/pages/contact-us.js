faf188ffde535b22f58608f888a50e5f05bb9060
Eleni <eleni.gesch-karamanlidis@gsa.gov>
2020-04-21 08:37:29 -0500

    Update contact-us.js

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -6,11 +6,12 @@ import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Contact us" />
-    <h1>Contact us</h1>
- <h2>Questions on how to use Site Scanner?</h2>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <SEO title="Contact us" /> 
+  <h1>Contact us</h1>
+  
+ <p>For general questions and comments about how Site Scanner works or scan results, please >a ref="mailto:site-scanning@gsa.gov">email the team</a>.</p>
+  
+  <p>If you have questions about analyzing and taking action on specific Site Scanner results, check out <a ref="https://digital.gov/services/directory/">Digital.gov's Registry of services</a> to find further points of contact.
   </Layout>
 )
 
