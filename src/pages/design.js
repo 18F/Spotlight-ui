cf2250716d2a1ca06c46c5cf4ecff107a6c1e605
Eleni <eleni.gesch-karamanlidis@gsa.gov>
2020-04-21 17:09:55 -0500

    Create design.js

---
+++ src/pages/design.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title="Design" />
+    <h1>Site Scanner scan results for website design</h1>
+
+
+  </Layout>
+);
+
+export default SecondPage;
