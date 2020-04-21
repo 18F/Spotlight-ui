c911e0ed0a4498f2729a6db714aac3f50f414542
Eleni <eleni.gesch-karamanlidis@gsa.gov>
2020-04-21 17:10:40 -0500

    Create components.js

---
+++ src/pages/components.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title="components" />
+    <h1>Site Scanner scan results for required components</h1>
+
+
+  </Layout>
+);
+
+export default SecondPage;
