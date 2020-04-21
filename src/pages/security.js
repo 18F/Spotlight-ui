c50fd9332406b8f48b331ec60ba2eaee22a98260
Eleni <eleni.gesch-karamanlidis@gsa.gov>
2020-04-21 17:11:38 -0500

    Create security.js

---
+++ src/pages/security.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title="Security" />
+    <h1>Site Scanner scan results for security</h1>
+
+
+  </Layout>
+);
+
+export default SecondPage;
