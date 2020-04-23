6c77b68f2d86f7bb8c682c49ebe06fba59371086
Eleni <eleni.gesch-karamanlidis@gsa.gov>
2020-04-23 15:43:40 -0500

    Create url-results.js

---
+++ src/pages/url-results.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title="Analytics" />
+    <h1>Site Scanner scan results for analytics</h1>
+
+
+  </Layout>
+);
+
+export default SecondPage;
