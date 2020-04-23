841dd508758cdf2cde79f9ce58433a93ec34109b
Matt Henry <matt.henry@gsa.gov>
2020-04-23 10:02:24 -0400

    add reporttype prop back in

--- src/pages/design.js
+++ src/pages/design.js
@@ -5,11 +5,16 @@ import Layout from '../components/layout';
 import ReportQueryProvider from '../components/report-query-provider';
 
 const columns = [
-  { title: `Domain`, accessor: (obj) => obj.domain },
-  { title: `Agency`, accessor: (obj) => obj.agency },
-  { title: `USWDS Indicators`, accessor: (obj) => obj.data.total_score },
+  { title: `Domain`, key: `domain`, accessor: (obj) => obj.domain },
+  { title: `Agency`, key: `agency`, accessor: (obj) => obj.agency },
+  {
+    title: `USWDS Indicators`,
+    key: `data.total_score`,
+    accessor: (obj) => obj.data.total_score,
+  },
   {
     title: `USWDS Version`,
+    key: `data.uswdsversion`,
     accessor: (obj) => obj.data.uswdsversion,
   },
 ];
