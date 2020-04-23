b7f8b5dc194e7fd5ca3f492abbace51c2499240a
Eleni <eleni.gesch-karamanlidis@gsa.gov>
2020-04-23 17:00:40 -0500

    Update security.js

--- src/pages/security.js
+++ src/pages/security.js
@@ -7,7 +7,7 @@ import SEO from '../components/seo';
 const SecondPage = () => (
   <Layout>
     <SEO title="Security" />
-    <h1>Site Scanner scan results for security</h1>
+    <h1>Security scan results</h1>
 
 
   </Layout>
