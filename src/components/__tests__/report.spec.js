d29f037413473743acdee9b87ed20be96b6d13e3
Matt Henry <matt.henry@gsa.gov>
2020-04-28 15:37:04 -0400

    Basic test running with mock axios

---
+++ src/components/__tests__/report.spec.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { render, act } from '@testing-library/react';
+import axiosMock from 'axios';
+import Report from '../report';
+import ReportQueryProvider from '../report-query-provider';
+import { API_BASE_URL } from '../../constants';
+
+jest.mock('axios');
+
+describe('Report', () => {
+  const columns = [
+    { title: `Domain`, accessor: (obj) => obj.domain },
+    { title: `Agency`, accessor: (obj) => obj.agency },
+    { title: `Supports HSTS`, accessor: (obj) => obj.data.HSTS },
+    { title: `Supports HTTPS`, accessor: (obj) => obj.data['HTTPS Live'] },
+    { title: `Headers`, accessor: (obj) => obj.data.endpoints.https.headers },
+  ];
+
+  const respObj = {
+    results: [
+      {
+        domain: '1.usa.gov',
+        scantype: 'pshtt',
+        domaintype: '',
+        organization: '',
+        agency: 'General Services Administration',
+        data: {
+          HSTS: true,
+          'HTTPS Live': true,
+          endpoints: {
+            https: {
+              headers: { Connection: 'keep-alive' },
+            },
+          },
+        },
+      },
+    ],
+  };
+
+  it('loads data from the API', async () => {
+    axiosMock.get.mockImplementation((url) => {
+      switch (url) {
+        case `${API_BASE_URL}lists/dates/`:
+          return { data: ['2020-04-20'] };
+        case `${API_BASE_URL}lists/pshtt/agencies`:
+          return { data: ['AMTRAK', 'Consumer Financial Protection Bureau'] };
+        default: {
+          return { data: respObj };
+        }
+      }
+    });
+    await act(async () => {
+      render(
+        <ReportQueryProvider>
+          <Report
+            columns={columns}
+            reportType={'security'}
+            endpoint={'scans/pshtt'}
+          />
+        </ReportQueryProvider>
+      );
+    });
+
+    expect(axiosMock.get).toHaveBeenCalledTimes(3);
+  });
+});
