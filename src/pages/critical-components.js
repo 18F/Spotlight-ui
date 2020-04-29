2b7162e8b2ef58dd7b524d19ee833a136392dde2
Matt Henry <matt.henry@gsa.gov>
2020-04-29 09:51:20 -0400

    Add external_domains to report

--- src/pages/critical-components.js
+++ src/pages/critical-components.js
@@ -3,10 +3,25 @@ import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import Report from '../components/report';
+import ReportQueryProvider from '../components/report-query-provider';
+
+const columns = [
+  { title: `Domain`, accessor: (obj) => obj.domain },
+  { title: `Agency`, accessor: (obj) => obj.agency },
+  { title: `External Domains`, accessor: (obj) => obj.data.external_domains },
+];
 
 export default () => (
   <Layout>
     <SEO title="Critical Components" />
     <h1>Critical components scan results</h1>
+    <ReportQueryProvider>
+      <Report
+        columns={columns}
+        endpoint={'scans/third_parties'}
+        reportType={'criticalComponents'}
+      />
+    </ReportQueryProvider>
   </Layout>
 );
