846b51811910ce6a30f871f10cafaa9497ac9c3b
Matt Henry <matt.henry@gsa.gov>
2020-05-12 12:54:52 -0400

    Add loading spinner. Closes 18F/site-scanning#517

--- src/components/report.js
+++ src/components/report.js
@@ -126,7 +126,13 @@ const ReportTableBody = ({ columns, records, isLoading, error }) => {
   return isLoading ? (
     <tbody>
       <tr>
-        <td data-testid="loading-table">Loading</td>
+        <td
+          className="loading-table"
+          data-testid="loading-table"
+          colSpan={columns.length}
+        >
+          <Spinner />
+        </td>
       </tr>
     </tbody>
   ) : records.length > 0 ? (
@@ -217,4 +223,44 @@ const ObjectList = ({ object }) => {
 
 ObjectList.propTypes = {
   record: PropTypes.object,
+};
+
+const Spinner = () => {
+  return (
+    <div className="loading-indicator">
+      <svg
+        version="1.1"
+        class="svg-loader"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        viewBox="0 0 80 80"
+        xmlSpace="preserve"
+        height="80"
+      >
+        <path
+          id="spinner"
+          fill="#2672de"
+          d="M40,72C22.4,72,8,57.6,8,40C8,22.4,
+		22.4,8,40,8c17.6,0,32,14.4,32,32c0,1.1-0.9,2-2,2
+		s-2-0.9-2-2c0-15.4-12.6-28-28-28S12,24.6,12,40s12.6,
+		28,28,28c1.1,0,2,0.9,2,2S41.1,72,40,72z"
+        >
+          <animateTransform
+            attributeType="xml"
+            attributeName="transform"
+            type="rotate"
+            from="0 40 40"
+            to="360 40 40"
+            dur="0.8s"
+            repeatCount="indefinite"
+          />
+        </path>
+      </svg>
+      <span role="alert" aria-busy="true">
+        Loading
+      </span>
+    </div>
+  );
 };
