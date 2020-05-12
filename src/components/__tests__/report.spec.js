5e68d216d5bf8d7cdb7f443934be0c158e7df1de
Matt Henry <matt.henry@gsa.gov>
2020-05-12 15:22:23 -0400

    Nest `describe`s to make output nicer

--- src/components/__tests__/report.spec.js
+++ src/components/__tests__/report.spec.js
@@ -32,201 +32,204 @@ const dateUrl = `${API_BASE_URL}lists/dates/`;
 const agencyUrl = `${API_BASE_URL}lists/pshtt/agencies`;
 const reportUrl = `${API_BASE_URL}scans/pshtt/?page=1`;
 
-describe('A <Report /> loading correctly', () => {
-  const respObj = {
-    count: 4914,
-    results: [
-      {
-        domain: '1.usa.gov',
-        scantype: 'pshtt',
-        domaintype: '',
-        organization: '',
-        agency: 'General Services Administration',
-        data: {
-          HSTS: true,
-          'HTTPS Live': true,
-          endpoints: {
-            https: {
-              headers: { Connection: 'keep-alive' },
+describe('A <Report>', () => {
+  describe('that loads correctly', () => {
+    const respObj = {
+      count: 4914,
+      results: [
+        {
+          domain: '1.usa.gov',
+          scantype: 'pshtt',
+          domaintype: '',
+          organization: '',
+          agency: 'General Services Administration',
+          data: {
+            HSTS: true,
+            'HTTPS Live': true,
+            endpoints: {
+              https: {
+                headers: { Connection: 'keep-alive' },
+              },
             },
           },
         },
-      },
-    ],
-  };
+      ],
+    };
 
-  let report;
+    let report;
 
-  beforeEach(async () => {
-    axiosMock.get.mockImplementation(url => {
-      switch (url) {
-        case dateUrl:
-          return { data: ['2020-04-20', '2020-04-21'] };
-        case agencyUrl:
-          return { data: ['AMTRAK', 'Consumer Financial Protection Bureau'] };
-        case reportUrl:
-          return { data: respObj };
-        default: {
-          return { data: { ...respObj, filtered: 'YES!' } };
+    beforeEach(async () => {
+      axiosMock.get.mockImplementation(url => {
+        switch (url) {
+          case dateUrl:
+            return { data: ['2020-04-20', '2020-04-21'] };
+          case agencyUrl:
+            return { data: ['AMTRAK', 'Consumer Financial Protection Bureau'] };
+          case reportUrl:
+            return { data: respObj };
+          default: {
+            return { data: { ...respObj, filtered: 'YES!' } };
+          }
         }
-      }
-    });
-    await act(async () => {
-      report = render(
-        <ReportQueryProvider>
-          <Report
-            columns={columns}
-            reportType={'security'}
-            endpoint={'scans/pshtt'}
-          />
-        </ReportQueryProvider>
-      );
-    });
-  });
-
-  it('calls the correct endpoints', () => {
-    expect(axiosMock.get).toHaveBeenCalledTimes(3);
-    expect(axiosMock.get).toHaveBeenCalledWith(dateUrl);
-    expect(axiosMock.get).toHaveBeenCalledWith(agencyUrl);
-    expect(axiosMock.get).toHaveBeenCalledWith(reportUrl);
-  });
-
-  it('incrementally applies the domain filter when that input changes', async () => {
-    const domainFilter = report.getByTestId('domain-filter');
-
-    act(() => {
-      fireEvent.change(domainFilter, {
-        target: { value: '18f' },
+      });
+      await act(async () => {
+        report = render(
+          <ReportQueryProvider>
+            <Report
+              columns={columns}
+              reportType={'security'}
+              endpoint={'scans/pshtt'}
+            />
+          </ReportQueryProvider>
+        );
       });
     });
 
-    await waitFor(() => {
-      expect(axiosMock.get).toHaveBeenLastCalledWith(
-        `${reportUrl}&domain=18f*`
-      );
+    it('calls the correct endpoints', () => {
+      expect(axiosMock.get).toHaveBeenCalledTimes(3);
+      expect(axiosMock.get).toHaveBeenCalledWith(dateUrl);
+      expect(axiosMock.get).toHaveBeenCalledWith(agencyUrl);
+      expect(axiosMock.get).toHaveBeenCalledWith(reportUrl);
     });
-  });
 
-  it('updates the query when the agency filter changes', async () => {
-    const filterUrl = `${reportUrl}&agency="Consumer+Financial+Protection+Bureau"`;
-    const agencyFilter = report.getByTestId('agency-filter');
+    it('incrementally applies the domain filter when that input changes', async () => {
+      const domainFilter = report.getByTestId('domain-filter');
 
-    // It applies a filter when an agency is selected
-    act(() => {
-      fireEvent.change(agencyFilter, {
-        target: { value: 'Consumer Financial Protection Bureau' },
+      act(() => {
+        fireEvent.change(domainFilter, {
+          target: { value: '18f' },
+        });
+      });
+
+      await waitFor(() => {
+        expect(axiosMock.get).toHaveBeenLastCalledWith(
+          `${reportUrl}&domain=18f*`
+        );
       });
     });
 
-    await waitFor(() => {
-      expect(axiosMock.get).toHaveBeenLastCalledWith(filterUrl);
-    });
+    it('updates the query when the agency filter changes', async () => {
+      const filterUrl = `${reportUrl}&agency="Consumer+Financial+Protection+Bureau"`;
+      const agencyFilter = report.getByTestId('agency-filter');
 
-    // It removes a filter when the agency is deselected
-    act(() => {
-      fireEvent.change(agencyFilter, {
-        target: { value: ' ' },
+      // It applies a filter when an agency is selected
+      act(() => {
+        fireEvent.change(agencyFilter, {
+          target: { value: 'Consumer Financial Protection Bureau' },
+        });
+      });
+
+      await waitFor(() => {
+        expect(axiosMock.get).toHaveBeenLastCalledWith(filterUrl);
+      });
+
+      // It removes a filter when the agency is deselected
+      act(() => {
+        fireEvent.change(agencyFilter, {
+          target: { value: ' ' },
+        });
+      });
+
+      await waitFor(() => {
+        expect(axiosMock.get).toHaveBeenLastCalledWith(reportUrl);
       });
     });
 
-    await waitFor(() => {
-      expect(axiosMock.get).toHaveBeenLastCalledWith(reportUrl);
-    });
-  });
+    it('applies the scan date filter', async () => {
+      const scanDateFilter = report.getByTestId('scan-date-filter');
 
-  it('applies the scan date filter', async () => {
-    const scanDateFilter = report.getByTestId('scan-date-filter');
+      act(() => {
+        fireEvent.change(scanDateFilter, {
+          target: { value: '2020-04-20' },
+        });
+      });
 
-    act(() => {
-      fireEvent.change(scanDateFilter, {
-        target: { value: '2020-04-20' },
+      await waitFor(() => {
+        expect(axiosMock.get).toHaveBeenLastCalledWith(
+          `${API_BASE_URL}date/2020-04-20/scans/pshtt/?page=1`
+        );
       });
     });
 
-    await waitFor(() => {
-      expect(axiosMock.get).toHaveBeenLastCalledWith(
-        `${API_BASE_URL}date/2020-04-20/scans/pshtt/?page=1`
-      );
+    it('updates the page when a pagination link is clicked', async () => {
+      const pageOneSpan = report.getByTestId('page-span-1');
+      const pageTwoLink = report.getByTestId('page-2');
+
+      expect(pageOneSpan).toHaveAttribute('aria-current', 'true');
+      expect(pageTwoLink).toHaveAttribute('aria-current', 'false');
+
+      act(() => {
+        fireEvent.click(pageTwoLink);
+      });
+
+      await waitFor(() => {
+        const pageTwoSpan = report.getByTestId('page-span-2');
+        expect(pageTwoSpan).toHaveAttribute('aria-current', 'true');
+      });
     });
   });
 
-  it('updates the page when a pagination link is clicked', async () => {
-    const pageOneSpan = report.getByTestId('page-span-1');
-    const pageTwoLink = report.getByTestId('page-2');
+  describe('that fails to load data from the API', () => {
+    afterEach(cleanup);
+    const respObj = {
+      count: 4914,
+      results: [
+        {
+          domain: '1.usa.gov',
+          scantype: 'pshtt',
+          domaintype: '',
+          organization: '',
+          agency: 'General Services Administration',
+          data: '',
+        },
+      ],
+    };
 
-    expect(pageOneSpan).toHaveAttribute('aria-current', 'true');
-    expect(pageTwoLink).toHaveAttribute('aria-current', 'false');
+    let report;
 
-    act(() => {
-      fireEvent.click(pageTwoLink);
-    });
-
-    await waitFor(() => {
-      const pageTwoSpan = report.getByTestId('page-span-2');
-      expect(pageTwoSpan).toHaveAttribute('aria-current', 'true');
-    });
-  });
-});
-
-describe('A <Report /> that fails to load data from the API', () => {
-  afterEach(cleanup);
-  const respObj = {
-    count: 4914,
-    results: [
-      {
-        domain: '1.usa.gov',
-        scantype: 'pshtt',
-        domaintype: '',
-        organization: '',
-        agency: 'General Services Administration',
-        data: '',
-      },
-    ],
-  };
-
-  let report;
-
-  beforeEach(async () => {
-    axiosMock.get.mockImplementation(url => {
-      switch (url) {
-        case dateUrl:
-          return { data: ['2020-04-20', '2020-04-21'] };
-        case agencyUrl:
-          return { data: ['AMTRAK', 'Consumer Financial Protection Bureau'] };
-        case reportUrl:
-          return '';
-        default: {
-          return { data: { ...respObj, filtered: 'YES!' } };
+    beforeEach(async () => {
+      axiosMock.get.mockImplementation(url => {
+        switch (url) {
+          case dateUrl:
+            return { data: ['2020-04-20', '2020-04-21'] };
+          case agencyUrl:
+            return { data: ['AMTRAK', 'Consumer Financial Protection Bureau'] };
+          case reportUrl:
+            return '';
+          default: {
+            return { data: { ...respObj, filtered: 'YES!' } };
+          }
         }
-      }
-    });
-    act(async () => {
-      report = render(
-        <ReportQueryProvider>
-          <Report
-            columns={columns}
-            reportType={'security'}
-            endpoint={'scans/pshtt'}
-          />
-        </ReportQueryProvider>
-      );
-    });
-  });
+      });
 
-  it('displays a loading displays a loading indicator', () => {
-    expect(report.getByTestId('loading-table')).toBeInTheDocument();
-  });
-
-  it('loads without crashing', async () => {
-    await waitFor(() => {
-      expect(report.queryByTestId('loading-table')).not.toBeInTheDocument();
-      expect(report.getByTestId('filter-form')).toBeInTheDocument();
+      act(async () => {
+        report = render(
+          <ReportQueryProvider>
+            <Report
+              columns={columns}
+              reportType={'security'}
+              endpoint={'scans/pshtt'}
+            />
+          </ReportQueryProvider>
+        );
+      });
     });
-  });
 
-  it('displays an error message', async () => {
-    await waitFor(() => {
-      expect(report.getByTestId('error-alert')).toBeInTheDocument();
+    it('displays a loading displays a loading indicator', () => {
+      expect(report.getByTestId('loading-table')).toBeInTheDocument();
+    });
+
+    it('loads without crashing', async () => {
+      await waitFor(() => {
+        expect(report.queryByTestId('loading-table')).not.toBeInTheDocument();
+        expect(report.getByTestId('filter-form')).toBeInTheDocument();
+      });
+    });
+
+    it('displays an error message', async () => {
+      await waitFor(() => {
+        expect(report.getByTestId('error-alert')).toBeInTheDocument();
+      });
     });
   });
 });
