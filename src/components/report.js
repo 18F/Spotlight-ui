37819140856706a6d1793c3bdccfc9bd2e76acf4
Matt Henry <matt.henry@gsa.gov>
2020-05-12 13:25:03 -0400

    class -> className

--- src/components/report.js
+++ src/components/report.js
@@ -230,7 +230,7 @@ const Spinner = () => {
     <div className="loading-indicator">
       <svg
         version="1.1"
-        class="svg-loader"
+        className="svg-loader"
         xmlns="http://www.w3.org/2000/svg"
         xmlnsXlink="http://www.w3.org/1999/xlink"
         x="0px"
