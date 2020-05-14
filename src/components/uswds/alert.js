5b5ad6d95500bc546a4ec6f415e39698bf4fe14d
Matt Henry <matt.henry@gsa.gov>
2020-05-14 09:46:03 -0400

    class -> className

--- src/components/uswds/alert.js
+++ src/components/uswds/alert.js
@@ -3,13 +3,13 @@ import React from 'react';
 export default ({ type, heading, message }) => {
   return (
     <div
-      class={`usa-alert usa-alert--${type}`}
+      className={`usa-alert usa-alert--${type}`}
       role="alert"
       data-testid={`alert-${type}`}
     >
-      <div class="usa-alert__body">
-        <h3 class="usa-alert__heading">{heading}</h3>
-        <p class="usa-alert__text">{message}</p>
+      <div className="usa-alert__body">
+        <h3 className="usa-alert__heading">{heading}</h3>
+        <p className="usa-alert__text">{message}</p>
       </div>
     </div>
   );
