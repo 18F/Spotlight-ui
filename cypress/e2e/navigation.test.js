af4c41d78835b6f579aed41d36a8b052ef0ec1d5
Matt Henry <matt.henry@gsa.gov>
2020-05-27 12:28:43 -0400

    basic navigation tests

--- cypress/e2e/navigation.test.js
+++ cypress/e2e/navigation.test.js
@@ -4,4 +4,22 @@ describe('Navigation', () => {
     cy.visit('http://localhost:8000/');
     cy.findByTestId('site-title');
   });
+
+  it('should load each report from the navigation', () => {
+    const reports = [
+      { text: 'Design', path: 'design' },
+      { text: 'Security', path: 'security' },
+      { text: 'Accessibility', path: 'accessibility' },
+      { text: 'Performance', path: 'performance' },
+      { text: 'Third-Party Links', path: 'critical-components' },
+    ];
+
+    reports.map(r => {
+      cy.visit('http://localhost:8000/');
+      cy.get('.usa-menu-btn').click();
+      cy.get('.usa-accordion__button.usa-nav__link').click();
+      cy.get('.usa-nav__submenu').contains(r.text).click();
+      cy.url().should('include', r.path);
+    });
+  });
 });
