8e700f420cc0d2017bfa62e825009fc010cdbaf5
Gray Brooks <gbinal@users.noreply.github.com>
2020-06-22 14:51:59 -0400

    adding in 2 links

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -8,19 +8,15 @@ const ContactUs = () => (
     <SEO title="Contact us" />
     <h1>Contact us</h1>
 
+  <p>
+  To learn more about the Site Scanning program and the methodology for each scan, the project's <a href="https://github.com/18F/site-scanning-documentation">documentation hub</a> is a good place to start.  
+  </p>
     <p>
       For general questions and comments about how Spotlight works or its scan
-      results, please <a href="mailto:spotlight@gsa.gov">email the team</a>.
+      results, please <a href="mailto:site-scanning@gsa.gov">email the team</a> or file an issue <a href="https://github.com/18F/Spotlight/issues">in the project repository</a>.
     </p>
 
-    <p>
-      If you have questions about analyzing and taking action on specific
-      Spotlight results, check out{' '}
-      <a href="https://digital.gov/services/directory/">
-        Digital.gov's Registry of services
-      </a>{' '}
-      to find further points of contact.
-    </p>
+
   </Layout>
 );
 
