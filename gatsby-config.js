fe208d5d1dc7b55bb6db1e4df687a89e550f9513
Robert Jolly <robert.jolly@gsa.gov>
2020-08-21 09:12:23 -0600

    add google tag manager
    Signed-off-by: Robert Jolly <robert.jolly@gsa.gov>

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,38 @@ module.exports = {
   },
   pathPrefix: process.env.BASEURL || '/',
   plugins: [
+    { 
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-MNT5VXS",
+  
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: function () {
+          return {
+            pageType: window.pageType,
+          }
+        },
+  
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        dataLayerName: "spotlight-ui",
+  
+        // Name of the event that is triggered
+        // on every Gatsby route change.
+        //
+        // Defaults to gatsby-route-change
+        routeChangeEventName: "spotlightui_route_change",
+      },
+    },
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
