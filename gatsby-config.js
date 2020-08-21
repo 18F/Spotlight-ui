f418cc305eea8bc1e44d3b91185ef959a44064c6
Robert Jolly <robert.jolly@gsa.gov>
2020-08-21 10:57:24 -0600

    comment out dataLayerName for GTM
    Signed-off-by: Robert Jolly <robert.jolly@gsa.gov>

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
         // Include GTM in development.
         //
         // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
+        includeInDevelopment: true,
   
         // datalayer to be set before GTM is loaded
         // should be an object or a function that is executed in the browser
@@ -29,7 +29,7 @@ module.exports = {
         // Specify optional GTM environment details.
         // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
         // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-        dataLayerName: "spotlight-ui",
+        // dataLayerName: "spotlight-ui",
   
         // Name of the event that is triggered
         // on every Gatsby route change.
