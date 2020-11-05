6849d1dde4b5c17347a629c08d368521a3271300
Eleni Chappen <eleni.chappen@gsa.gov>
2020-11-05 10:00:12 -0600

    Add visual indicator for filterable vs non-filterable fields Since filter fields are hidden until field is selected, user should know which fields can be filtered up-front. places icon of filter next to filterable field checkbox labels

--- src/components/available-field.js
+++ src/components/available-field.js
@@ -1,41 +1,56 @@
-import React from 'react'; // eslint-disable-line
-import PropTypes from 'prop-types';
-import Checkbox from './uswds/checkbox';
-import TextInput from './uswds/text-input';
-import Dropdown from './uswds/dropdown';
+import React               from 'react'; // eslint-disable-line
+import PropTypes           from 'prop-types';
+import Checkbox            from './uswds/checkbox';
+import TextInput           from './uswds/text-input';
+import Dropdown            from './uswds/dropdown';
+import { faFilter }        from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const AvailableField = (props) => {
     const { attribute, title, input, value, input_options } = props.field;
     return (
         <div>
-            <Checkbox
-                key={attribute}
-                id={`select_${attribute}`}
-                label={title}
-                name={attribute}
-                checked={props.checked}
-                onChange={props.onSelectChange}
-            />
+            <div className='display-flex flex-align-center'>
+                <Checkbox
+                    key={attribute}
+                    id={`select_${attribute}`}
+                    label={title}
+                    name={attribute}
+                    checked={props.checked}
+                    onChange={props.onSelectChange}
+                />
+                { input &&
+                    <FontAwesomeIcon
+                        icon={faFilter}
+                        style={{
+                            width: '13px',
+                            marginBottom: '10px',
+                            marginLeft: '.5rem',
+                        }}
+                        title='This field is filterable'
+                    />
+                }
+            </div>
             { props.checked && input &&
                 <div className='margin-left-4 margin-top-1 margin-bottom-2'>
-                { input === 'text' &&
-                    <TextInput
-                        id={`filter_by_${attribute}`}
-                        name={`filter_by_${attribute}`}
-                        placeholder={`Filter by ${title}`}
-                        value={value}
-                        onChange={props.onFieldChange}
-                    />
-                }
-                { input === 'select' &&
-                    <Dropdown
-                        id={`filter_by_${attribute}`}
-                        name={`filter_by_${attribute}`}
-                        value={value}
-                        onChange={props.onFieldChange}
-                        options={input_options}
-                    />
-                }
+                    { input === 'text' &&
+                        <TextInput
+                            id={`filter_by_${attribute}`}
+                            name={`filter_by_${attribute}`}
+                            placeholder={`Filter by ${title}`}
+                            value={value}
+                            onChange={props.onFieldChange}
+                        />
+                    }
+                    { input === 'select' &&
+                        <Dropdown
+                            id={`filter_by_${attribute}`}
+                            name={`filter_by_${attribute}`}
+                            value={value}
+                            onChange={props.onFieldChange}
+                            options={input_options}
+                        />
+                    }
                 </div>
             }
         </div>
--- src/components/uswds/text-input.js
+++ src/components/uswds/text-input.js
@@ -5,7 +5,6 @@ const TextInput = (props) => {
     return (
         <input
             className="usa-input"
-            // aria-describedby="with-hint-input-info with-hint-input-hint"
             id={props.id}
             name={props.name}
             onChange={props.onChange}
