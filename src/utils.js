bfb23e963de5c93058a6fc7d8d15f92da3a362c9
Eleni Chappen <eleni.chappen@gsa.gov>
2020-11-06 12:22:12 -0600

    include filters in url params

--- src/utils.js
+++ src/utils.js
@@ -27,12 +27,14 @@ export const buildQueryParams = (obj) => {
 }
 
 export const parseFieldParams = (string, param) => {
-  const value = new URLSearchParams(string).getAll(param);
+  const searchParams = new URLSearchParams(string);
+  const value = searchParams.getAll(param);
   return (value[0] || "")
     .split(',')
     .filter(val => val.length)
     .reduce((acc, val) => {
       acc[val] = FIELD_OPTIONS[val];
+      if (searchParams.get(val)) acc[val]['value'] = searchParams.get(val);
       return acc;
     }, {});
 
