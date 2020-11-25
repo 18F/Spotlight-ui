3428dfdee21efd7cc54525cff57d225540aa66d8
Eleni Chappen <eleni.chappen@gsa.gov>
2020-11-25 14:33:17 -0600

    add initial API domain and endpoint

--- src/data/api.js
+++ src/data/api.js
@@ -1,5 +1,5 @@
-export const API_DOMAIN = 'https://example-api.com';
+export const API_DOMAIN = ' https://api.gsa.gov/technology/site-scanning';
 
-export const API_PATH = '/example-endpoint';
+export const API_PATH = '/v1/websites';
 
 export const API_KEY = 'DEMO_KEY';
